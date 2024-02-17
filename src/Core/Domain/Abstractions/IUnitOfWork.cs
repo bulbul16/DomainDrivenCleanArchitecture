b3b326c57c4698ce5ff22285282b793414f06a63
bulbul <bulbul.cse@outlook.com>
2024-02-18 02:15:04 +0600

    application layer abstractions classes added

--- src/Core/Domain/Abstractions/IUnitOfWork.cs
+++ src/Core/Domain/Abstractions/IUnitOfWork.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Domain.Abstractions;
 
-namespace Domain.Abstractions
+public interface IUnitOfWork
 {
-    internal interface IUnitOfWork
-    {
-    }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
